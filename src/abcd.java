bf19f221f2d4bb2a1240f5feefe98a4555ebeb7e
Anurag <anuragp067@gmail.com>
2022-03-13 09:20:30 +0530

    ..

---
+++ src/abcd.java
@@ -0,0 +1,9 @@
+
+  import java.util.Scanner;
+public class abcd {
+  
+    public static void main(String[] args)
+    {
+    System.out.println("mera naam anurag hai ");
+    }
+}
--- src/output.java
+++ src/output.java
@@ -1,26 +1,42 @@
-import java.security.Identity;
+// import java.security.Identity;
+// import java.util.Scanner;
+
+// class Anurag22 {
+
+//     public int id;
+//     public String name;
+
+//     public Anurag22() {
+//       id=45;
+//       name="anurag";
+//       System.out.println(name);
+//     }
+
+// }
+
+// public class output {
+//     public static void main(String[] args) {
+
+//        Anurag22 hb =new Anurag22();
+
+//     }
+// }
+
 import java.util.Scanner;
 
-class Anurag22 {
-
-    public int id;
-    public String name;
-
-    public Anurag22() {
-      id=45;
-      name="anurag";
-      System.out.println(name);
-    }
-
-   
-}
-
 public class output {
+    int a;
+
     public static void main(String[] args) {
-
-       Anurag22 hb =new Anurag22();
-    
-
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the year");
+        int a = sc.nextInt();
+        if ((a % 400 == 0) || (a % 4 == 0) && (a % 100 != 0)) {
+            System.out.println("It is leap year");
+        }
+            else{
+                System.out.println("not the leap year");
+            
+        }
     }
 }
